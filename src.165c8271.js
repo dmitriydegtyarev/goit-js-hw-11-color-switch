444e67192094ecf95a36c4f53dc47f58bae74889
dmitriydegtyarev <61326282+dmitriydegtyarev@users.noreply.github.com>
2021-05-17 17:20:00 +0000

    Deploying to gh-pages from @ dmitriydegtyarev/goit-js-hw-11-color-switch@a47a6435641e31898dcec403d54ec093635f3be9 🚀

---
+++ src.165c8271.js
@@ -0,0 +1,12 @@
+parcelRequire=function(e,r,t,n){var i,o="function"==typeof parcelRequire&&parcelRequire,u="function"==typeof require&&require;function f(t,n){if(!r[t]){if(!e[t]){var i="function"==typeof parcelRequire&&parcelRequire;if(!n&&i)return i(t,!0);if(o)return o(t,!0);if(u&&"string"==typeof t)return u(t);var c=new Error("Cannot find module '"+t+"'");throw c.code="MODULE_NOT_FOUND",c}p.resolve=function(r){return e[t][1][r]||r},p.cache={};var l=r[t]=new f.Module(t);e[t][0].call(l.exports,p,l,l.exports,this)}return r[t].exports;function p(e){return f(p.resolve(e))}}f.isParcelRequire=!0,f.Module=function(e){this.id=e,this.bundle=f,this.exports={}},f.modules=e,f.cache=r,f.parent=o,f.register=function(r,t){e[r]=[function(e,r){r.exports=t},{}]};for(var c=0;c<t.length;c++)try{f(t[c])}catch(e){i||(i=e)}if(t.length){var l=f(t[t.length-1]);"object"==typeof exports&&"undefined"!=typeof module?module.exports=l:"function"==typeof define&&define.amd?define(function(){return l}):n&&(this[n]=l)}if(parcelRequire=f,i)throw i;return f}({"krre":[function(require,module,exports) {
+
+},{}],"oLA8":[function(require,module,exports) {
+"use strict";Object.defineProperty(exports,"__esModule",{value:!0}),exports.default=void 0;var e=["#FFFFFF","#2196F3","#4CAF50","#FF9800","#009688","#795548"],F=e;exports.default=F;
+},{}],"VyiV":[function(require,module,exports) {
+"use strict";Object.defineProperty(exports,"__esModule",{value:!0}),exports.refs=void 0;var e={body:document.querySelector("body"),startBtn:document.querySelector("[data-action='start'"),stopBtn:document.querySelector("[data-action='stop'")};exports.refs=e;
+},{}],"SbnA":[function(require,module,exports) {
+"use strict";var o=l(require("./colors")),e=require("./refs");function l(o){return o&&o.__esModule?o:{default:o}}var n=e.refs.body,r=e.refs.startBtn,s=e.refs.stopBtn;console.log(o.default),console.log(e.refs),console.log(n),console.log(r),console.log(s),r.addEventListener("click",function(){console.log("Нажали кнопку старт"),console.log(n.style),n.style.backgroundColor="black",console.log(t(0,6))}),s.addEventListener("click",function(){console.log("Нажали кнопку стоп")});var t=function(o,e){return Math.floor(Math.random()*(e-o+1)+o)};
+},{"./colors":"oLA8","./refs":"VyiV"}],"Focm":[function(require,module,exports) {
+"use strict";require("./css/styles.css"),require("./js/color-switch");
+},{"./css/styles.css":"krre","./js/color-switch":"SbnA"}]},{},["Focm"], null)
+//# sourceMappingURL=/parcel-project-template/src.165c8271.js.map
