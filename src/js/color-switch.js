17ba0569bdd561c3158edc706cd5cff1d967b82b
Dmitriy Dihtiarov <dmitriydegtyarev1985@gmail.com>
2021-05-17 23:32:00 +0300

    finish homework

--- src/js/color-switch.js
+++ src/js/color-switch.js
@@ -3,28 +3,39 @@ import { refs } from './refs';
 
 const { body, startBtn, stopBtn } = refs;
 
-console.log(colors);
-console.log(refs);
-console.log(body);
-console.log(startBtn);
-console.log(stopBtn);
-
-startBtn.addEventListener('click', () => {
-  // startChangeColor
-  console.log('Нажали кнопку старт');
-  console.log(body.style);
-  body.style.backgroundColor = 'black';
-
-  console.log(randomIntegerFromInterval(0, 6));
-  // body.insertAdjacentHTML('beforebegin', 'style="background-color: black;"')
-});
-stopBtn.addEventListener('click', () => {
-  // stopChangeColor
-  console.log('Нажали кнопку стоп');
- });
+const colorsMaxIndex = colors.length - 1;
+let intervalID = null;
+let changeColor = false;
 
 const randomIntegerFromInterval = (min, max) => {
   return Math.floor(Math.random() * (max - min + 1) + min);
 };
 
-// style="background-color: black;"
+function initRandomColor() {
+  return body.style.backgroundColor = colors[randomIntegerFromInterval(0, colorsMaxIndex)];
+};
+
+function startChangeBackgroundColor() {
+
+  if (changeColor) {
+      return
+    };
+
+  intervalID = setInterval(() => {
+    initRandomColor();
+    startBtn.classList.add('visually-hidden');
+    changeColor = true;
+    console.log(initRandomColor(), changeColor);
+  }, 1000);  
+};
+
+function stopChangeBackgroundColor() {
+  clearInterval(intervalID);
+  body.style.backgroundColor = '';
+  startBtn.classList.remove('visually-hidden');
+  changeColor = false;
+  console.log(changeColor);
+};
+
+startBtn.addEventListener('click', startChangeBackgroundColor);
+stopBtn.addEventListener('click', stopChangeBackgroundColor);
